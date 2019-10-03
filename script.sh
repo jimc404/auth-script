33af59538895f1f503b5940e014643153b56dd94
Mayank Chandra <32404847+jimc404@users.noreply.github.com>
2019-10-03 15:57:01 +0530

    Reconnects on failure (can take time)

--- script.sh
+++ script.sh
@@ -3,21 +3,24 @@
 username="uname"   # replace uname with your username 
 password="passwd"  # replace passwd with your password
 
-magic=$(curl -s -k "https://agnigarh.iitg.ac.in:1442/login?" | grep "name=\"magic\"" | grep -oP 'value="\K[^"]*')
+while true
+	do
+		magic=$(curl -s -k "https://agnigarh.iitg.ac.in:1442/login?" | grep "name=\"magic\"" | grep -oP 'value="\K[^"]*')
 
-auth=$(curl -s -k  "https://agnigarh.iitg.ac.in:1442/" --data "4Tredir=https%3A%2F%2Fagnigarh.iitg.ac.in%3A1442%2Flogin%3F&magic=$magic&username=$username&password=$password" --compressed | grep -Eo "https:\/\/agnigarh.iitg.ac.in:1442\/keepalive\?[A-Za-z0-9]+")
+		auth=$(curl -s -k  "https://agnigarh.iitg.ac.in:1442/" --data "4Tredir=https%3A%2F%2Fagnigarh.iitg.ac.in%3A1442%2Flogin%3F&magic=$magic&username=$username&password=$password" --compressed | grep -Eo "https:\/\/agnigarh.iitg.ac.in:1442\/keepalive\?[A-Za-z0-9]+")
+		echo "Auth started"
+		status="true"
 
-echo "Auth started"
-status="true"
-
-while [ "$status" = "true" ]  
-    do  
-        alive=$(curl -s -k "$auth" | grep -o "$auth")  
-        if [ "$alive" != "$auth" ]
-        then
-            status="false"
-            echo "Auth failed"
-        else
-            sleep 500
-        fi
-    done
+		while [ "$status" = "true" ]  
+		    do  
+		        alive=$(curl -s -k "$auth" | grep -o "$auth")  
+		        if [ "$alive" != "$auth" ]
+		        then
+		            status="false"
+		            echo "Auth failed"
+		        else
+		            sleep 150
+		        fi
+		done
+		sleep 30
+	done
